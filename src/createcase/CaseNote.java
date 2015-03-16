e83c32b1b9e5e18a01fb6fe694a9a464cd5f71cc
Lewis-Evans <LEWIS.S.EVANS@STU.MMU.AC.UK>
2015-03-16 15:35:50 +0000

    Removed various imports from classes which are no longer needed

--- src/createcase/CaseNote.java
+++ src/createcase/CaseNote.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package createcase;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
